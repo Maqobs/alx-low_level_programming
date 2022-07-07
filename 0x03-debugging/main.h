3a567939561c2b0349bc2ff909f676d2101c55d7
Maqobs <41755329+Maqobs@users.noreply.github.com>
2022-07-07 20:31:04 +0200

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,15 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
-#ifndef HEADER_FILE
-#define HEADER_FILE
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_day(int, int month, int day, int year);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
 int convert_day(int month, int day);
 
-int _putchar(char c);
-
-#endif
 #endif /* MAIN_H */
