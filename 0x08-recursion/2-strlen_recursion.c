f2e9cf92e091e43a4e1d8acbe247a8043d235939
Maqobs <41755329+Maqobs@users.noreply.github.com>
2022-07-21 11:30:51 +0200

    done ?

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,16 @@
-nclude "main.h" 
-  
- /** 
-  *   * _strlen_recursion - Returns Length of String 
-  *    * @s: string 
-  *     * 
-  *      * Return: On success 1. 
-  *       * On error, -1 is returned, and errno is set appropriately. 
-  *        */ 
- int _strlen_recursion(char *s)
-{
-		 if (*s != '\0')
-		  {
-				   return (1 + _strlen_recursion(s + 1)); 
+#include "main.h"
 
-			}
-		  return (0);
-	 }
+/**
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursion
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
