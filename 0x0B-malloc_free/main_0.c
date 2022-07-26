d0562809e854ff22ab8511e74cb9364fe0a46873
Maqobs <41755329+Maqobs@users.noreply.github.com>
2022-07-26 14:16:31 +0200

    sentence error

--- 0x0B-malloc_free/main_0.c
+++ 0x0B-malloc_free/main_0.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *_strdup(char *str);
-
-/**
- * main - check the code .
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char *s;
-
-	s = _strdup("Best School!");
-	if (s == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (1);
-	}
-	printf("%s\n", s);
-	free(s);
-
-
 char *str_concat(char *, char *);
 
 /**
@@ -42,5 +21,4 @@ int main(void)
 	printf("%s\n", s);
 	free(s);
 	return (0);
-
 }
