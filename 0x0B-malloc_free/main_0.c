b2bbac36fb584af7f6b6cc1abbd7cdd639ab035f
Maqobs <41755329+Maqobs@users.noreply.github.com>
2022-07-26 14:09:14 +0200

    sebenzing

--- 0x0B-malloc_free/main_0.c
+++ 0x0B-malloc_free/main_0.c
@@ -20,5 +20,27 @@ int main(void)
 	}
 	printf("%s\n", s);
 	free(s);
+
+
+char *str_concat(char *, char *);
+
+/**
+ * main - check the code .
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s;
+
+	s = str_concat("Best ", "School");
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
 	return (0);
+
 }
