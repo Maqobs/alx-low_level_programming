c82ded75b0979796d6f71bc9d26ad19d6517b11f
Maqobs <41755329+Maqobs@users.noreply.github.com>
2022-07-27 13:34:49 +0200

    summer time

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check the code for alx School students.
+ * @argv: The character to print
+ * @argc: The character to print
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
