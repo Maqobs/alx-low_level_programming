209a3f71b433e580221797260c87db54b1f7761b
Maqobs <41755329+Maqobs@users.noreply.github.com>
2022-08-02 18:55:34 +0200

    does not commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+nes (18 sloc)  255 Bytes
+/*
+ * File: 5-free_dog.c
+ */
+
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees dogs.
+ * @d: The dog to be freed.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
