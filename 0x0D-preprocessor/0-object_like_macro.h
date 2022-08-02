9e266a306be173cdaa145db328af5ea304c237af
Maqobs <41755329+Maqobs@users.noreply.github.com>
2022-08-02 14:04:58 +0200

    mispelling

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,7 +1,6 @@
-#ifndef _OBJECT_LIKE_MACRO
-#define SIZE _OBJECT_LIKE_MACRO_
+#ifndef _OBJECT_LIKE_MACRO_
+#define _OBJECT_LIKE_MACRO_
 
 #define SIZE "1024"
 
-
-#endif /* _object_like_macro_h_ */
+#endif
